3e38fa2b94b4381112e36a934029bad890cc5605
Denali Schlesinger <89662251+dschlesinger@users.noreply.github.com>
2026-02-17 18:31:56 -0500

    Add NN lecture code

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello World")
+import numpy as np
+
+print(np.__version__)
+
+input = np.array([1, .5, -1, 0])
+weights = np.array([
+    [3, 2, 0, 0],
+    [-1, -2, -3, -1],
+    [1, 3, 2, -2.]
+])
+bias = np.array([1, .5, -1])
+
+def relu(x: np.array) -> np.array:
+  x[x < 0] = 0
+
+  return x
+
+print(relu(weights @ input + bias))
